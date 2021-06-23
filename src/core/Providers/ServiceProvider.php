ea95eaf9ad5e780b0a22ffd2eac32eee18e56cfb
Mr.L <admin@duxphp.com>
2021-06-23 10:50:41 +0800

    update

--- src/core/Providers/ServiceProvider.php
+++ src/core/Providers/ServiceProvider.php
@@ -2,9 +2,31 @@
 
 namespace Duxravel\Core\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 class ServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \Route::macro('manage', function ($class, $name = '') {
+            return (new \Duxravel\Core\Util\Route($class, $name));
+        });
+        app_hook('Service', 'App', 'extend');
+    }
 }
