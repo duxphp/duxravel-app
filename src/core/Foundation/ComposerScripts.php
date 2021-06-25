a58321dfe9b6bfa723e887d3ea30f984c7a8b5f5
Mr.L <admin@duxphp.com>
2021-06-25 12:29:38 +0800

    优化钩子缓存

--- src/core/Foundation/ComposerScripts.php
+++ src/core/Foundation/ComposerScripts.php
@@ -15,7 +15,7 @@ class ComposerScripts
     protected static function clear()
     {
         $laravel = new Application(getcwd());
-        if (is_file($servicesPath = $laravel->bootstrapPath('cache/duxravel'))) {
+        if (is_file($servicesPath = $laravel->bootstrapPath('cache/duxravel.php'))) {
             @unlink($servicesPath);
         }
     }
