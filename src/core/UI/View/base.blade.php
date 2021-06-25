b1174315e8244dac84f23bf83ae4b39d57fa5c9a
Mr.L <admin@duxphp.com>
2021-06-25 14:05:24 +0800

    优化钩子缓存

--- src/core/UI/View/base.blade.php
+++ src/core/UI/View/base.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>{{config('app.name')}}</title>
+    <title>{{config('app.name')}} {{config('theme.title')}}</title>
+
     <meta name="msapplication-TileColor" content="#206bc4"/>
     <meta name="theme-color" content="#206bc4"/>
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
