8889bbd81222e9d5a3ed8a4f7e5c4650fe890445
Mr.L <admin@duxphp.com>
2021-06-26 13:51:33 +0800

    update

--- src/core/Views/error.blade.php
+++ src/core/Views/error.blade.php
@@ -33,7 +33,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M10 19l-7-7m0 0l7-7m-7 7h18"/>
                     </svg>
-                    <div>返回上一页</div>
+                    <div>返回</div>
                 </a>
             </div>
         </div>
