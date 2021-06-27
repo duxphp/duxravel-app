4262e215b775671a871159f289da77897d01800d
Mr.L <admin@duxphp.com>
2021-06-27 21:13:46 +0800

    update

--- database/migrations/2021_06_26_114055_create_form_table.php
+++ database/migrations/2021_06_26_114055_create_form_table.php
@@ -14,9 +14,9 @@ class CreateFormTable extends Migration
     public function up()
     {
         Schema::table('form', function (Blueprint $table) {
-            $table->boolean('audit')->default(0)->comment('审核状态')->after('search');
-            $table->boolean('submit')->default(0)->comment('外部提交')->after('audit');
-            $table->integer('interval')->default(10)->comment('提交间隔')->comment('search');
+            $table->boolean('audit')->default(0)->nullable()->comment('审核状态')->after('search');
+            $table->boolean('submit')->default(0)->nullable()->comment('外部提交')->after('audit');
+            $table->integer('interval')->default(10)->nullable()->comment('提交间隔')->comment('search');
             $table->string('tpl_list')->nullable()->comment('列表模板')->after('submit');
             $table->string('tpl_info')->nullable()->comment('详情模板')->after('tpl_list');
         });
