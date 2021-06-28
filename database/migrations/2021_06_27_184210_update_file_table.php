373bf942b77b7a9c8e1efa98f7c51d163334ffbc
Mr.L <admin@duxphp.com>
2021-06-28 10:27:51 +0800

    update

--- database/migrations/2021_06_27_184210_update_file_table.php
+++ database/migrations/2021_06_27_184210_update_file_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFileTable extends Migration
+class UpdateFileTable extends Migration
 {
     /**
      * Run the migrations.
