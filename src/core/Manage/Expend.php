1f4b198656ce9a7101823fb88a7d859ddff69263
Mr.L <admin@duxphp.com>
2021-06-28 14:20:46 +0800

    update

--- src/core/Manage/Expend.php
+++ src/core/Manage/Expend.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
  * Trait Expend
  * @package Duxravel\Core\Controller
  * @method \Duxravel\Core\UI\Table table()
- * @method \Duxravel\Core\UI\Form form($id = 0):
+ * @method \Duxravel\Core\UI\Form form($id = 0)
  * @method storeData($data, $id)
  * @method delData($id = 0)
  * @method clearData($id, $info)
