8dab20ba634b99e0a3a04659cb6ff563435c29c7
Mr.L <admin@duxphp.com>
2021-06-29 09:21:24 +0800

    update

--- src/core/Middleware/Web.php
+++ src/core/Middleware/Web.php
@@ -9,6 +9,14 @@ class Web
 
     public function handle($request, Closure $next)
     {
+        $agent = new \Jenssegers\Agent\Agent();
+        $theme = config('theme.default');
+        $mobileTheme = config('theme.mobile');
+        if ($mobileTheme && ($agent->isMobile() || $agent->isTablet())) {
+            $theme = $mobileTheme;
+        }
+        $finder = app('view')->getFinder();
+        $finder->prependLocation(base_path('public/themes/' . $theme));
         return $next($request);
     }
 
