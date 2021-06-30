6489d010f7aef87d90e84162766911921f7d31f8
Mr.L <admin@duxphp.com>
2021-06-30 15:31:51 +0800

    update

--- src/core/Util/Upload.php
+++ src/core/Util/Upload.php
@@ -135,7 +135,7 @@ class Upload
                         $watermark = Image::make($source)->opacity($alpha);
                         $image->insert($watermark, $position, 10, 10);
                     }
-                    file_put_contents($tmpPath, $image->encode($ext, 100));
+                    \File::put($tmpPath, $image->encode($ext, 100));
                 }
                 $path = $file->store('upload/' . date('Y-m-d'), $driver);
                 if ($path) {
