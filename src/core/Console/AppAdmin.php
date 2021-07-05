439771b5a960ea71d3c32d001971aa9db06b85c8
Mr.L <admin@duxphp.com>
2021-07-05 13:13:28 +0800

    update

--- src/core/Console/AppAdmin.php
+++ src/core/Console/AppAdmin.php
@@ -12,7 +12,7 @@ class AppAdmin extends \Duxravel\Core\Console\Common\Stub
      *
      * @var string
      */
-    protected $signature = 'app:make-admin {name}';
+    protected $signature = 'app:make-admin {name} {--title= : 功能名称}';
 
     /**
      * The console command description.
@@ -39,18 +39,21 @@ class AppAdmin extends \Duxravel\Core\Console\Common\Stub
     public function handle()
     {
         $name = $this->argument('name');
+        $title = $this->option('title');
         $app = ucfirst($name);
-        if (!is_dir(base_path('/modules/'.$app))) {
+        if (!is_dir(base_path('/modules/' . $app))) {
             $this->error('应用不存在，请检查!');
             exit;
         }
         $fun = lcfirst($this->getAppName('请输入类名'));
         $class = ucfirst($fun);
-        $title = $this->ask('请输入功能名称');
+        if (!$title) {
+            $title = $this->ask('请输入功能名称');
+        }
         $modelClass = '\Duxravel\Core\Model\Base';
 
         // 创建控制器
-        $this->generatorFile($app."/Admin/{$class}.php", __DIR__.'/Tpl/AppAdmin/Admin.stub', [
+        $this->generatorFile($app . "/Admin/{$class}.php", __DIR__ . '/Tpl/AppAdmin/Admin.stub', [
             'app' => $app,
             'title' => $title,
             'class' => $class,
@@ -58,34 +61,46 @@ class AppAdmin extends \Duxravel\Core\Console\Common\Stub
         ]);
 
         // 创建路由
-        $routeFile = base_path('/modules/'.$app.'/Route/AuthAdmin.php');
-        $routeContent = file_get_contents($routeFile);
-        $routeContent = str_replace('    // Generate Route Make',
-            <<<PHP
-                    Route::group([
-                        'auth_group' => '$title'
-                    ], function () {
-                        Route::manage(\\Modules\\$app\Admin\\$class::class)->make();
-                    });
-                    // Generate Route Make
-                PHP
-            , $routeContent);
-        file_put_contents($routeFile, $routeContent);
+        $routeFile = base_path('/modules/' . $app . '/Route/AuthAdmin.php');
+
+        $this->appendFile($routeFile, <<<EOL
+                                    Route::group([
+                                        'auth_group' => '$title'
+                                    ], function () {
+                                        Route::manage(\\Modules\\$app\Admin\\$class::class)->make();
+                                    });
+                                    EOL,
+            '// Generate Route Make');
 
         // 创建菜单
-        $menuFile = base_path('/modules/'.$app.'/Service/Menu.php');
-        $menuContent = file_get_contents($menuFile);
-        $menuContent = str_replace('                            // Generate Menu Make',
-            <<<EOL
-                        [
-                            'name'  => '$title',
-                            'url'   => 'admin.$name.$fun',
-                        ],
-                        // Generate Menu Make
-                    EOL, $menuContent);
-        file_put_contents($menuFile, $menuContent);
+        $menuFile = base_path('/modules/' . $app . '/Service/Menu.php');
+
+        $this->appendFile($menuFile, <<<EOL
+                                    [
+                                        'name'  => '$title',
+                                        'url'   => 'admin.$name.$fun',
+                                    ],
+                                    EOL,
+            '// Generate Menu Make');
 
         $this->info('创建模型成功');
+    }
+
+    public function appendFile($file, $content = '', $mark = '')
+    {
+        $file = base_path('/modules/' . $file);
+        $data = [];
+        $contentData = explode("\n", $content);
+        foreach (file($file) as $line) {
+            if (strpos($file, $mark) !== false) {
+                $place = substr($line, 0, strrpos($line, $mark));
+                foreach ($contentData as $content) {
+                    $data[] = $place . $content;
+                }
+            }
+            $data[] = $line;
+        }
+        file_put_contents($file, implode("\n", $data));
     }
 
 }
