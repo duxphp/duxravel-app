58095c35d0e2e4feaa4a44c1dddab5116e96719f
Mr.L <admin@duxphp.com>
2021-07-07 21:09:42 +0800

    增加递归树形类

---
+++ src/core/Traits/Tree.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Duxravel\Core\Traits;
+
+/**
+ * Class Tree
+ * @package Duxravel\Core\Model
+ */
+trait Tree
+{
+    /**
+     * 获取下级
+     * @return mixed
+     */
+    public function children()
+    {
+        return $this->hasMany(get_class($this), 'parent_id');
+    }
+
+    /**
+     * 获取所有下级
+     * @return mixed
+     */
+    public function allChildren()
+    {
+        return $this->children()->with('allChildren');
+    }
+
+    /**
+     * 获取上级
+     * @return mixed
+     */
+    public function parent()
+    {
+        return $this->belongsTo(get_class($this), 'parent_id');
+    }
+
+    /**
+     * 获取所有上级
+     * @return mixed
+     */
+    public function allParent()
+    {
+        return $this->parent()->with('parent_rec');
+    }
+
+}
