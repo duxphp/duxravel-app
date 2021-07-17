60364c74b38e383b9bb740a5c7416a1069b483f4
Mr.L <admin@duxphp.com>
2021-07-17 11:53:09 +0800

    修改边栏样式

--- src/core/UI/View/Widget/tree-list.blade.php
+++ src/core/UI/View/Widget/tree-list.blade.php
@@ -1,10 +1,8 @@
-<div class="w-44">
-    <div class="flex-none bottom-0 top-14 fixed bottom-0 overflow-auto w-44  bg-white fixed px-2 pt-1 border-gray-300 border-solid border-r ">
-        <div class="text-xs text-gray-500 py-3">{{$title}}</div>
-        <div>
-            <ul>
-                @include('vendor.duxphp.duxravel-app.src.core.UI.View.Widget.tree-list-inner', ['items' => $data, 'level' => 0])
-            </ul>
-        </div>
+<div class="w-44 flex flex-col bg-white rounded shadow">
+    <div class="p-4 flex-none border-b border-gray-300">{{$title}}</div>
+    <div class="flex-grow overflow-y-hidden">
+        <ul>
+            @include('vendor.duxphp.duxravel-app.src.core.UI.View.Widget.tree-list-inner', ['items' => $data, 'level' => 0])
+        </ul>
     </div>
 </div>
--- src/core/UI/View/table.blade.php
+++ src/core/UI/View/table.blade.php
@@ -1,159 +1,163 @@
-<div class="lg:flex">
-    @foreach($sideLeftHtml as $vo)
-        <div class="lg:flex-none hidden lg:block">
-            {!! $vo !!}
-        </div>
-    @endforeach
-
-    <div class="p-5 lg:flex-grow lg:w-44">
-        @if($title || $actions)
-            <div class="flex items-center  pb-4">
-                @if($title)
-                    <div class="flex-grow">
-                        <div class="text-xl">{{$title}}</div>
-                    </div>
-                @endif
-                @if ($actions)
-                    <div class="flex-none flex gap-2">
-                        @foreach ($actions as $vo)
-                            <div>{!! $vo !!}</div>
-                        @endforeach
-                    </div>
-                @endif
-            </div>
-        @endif
-
-        @if($headerHtml)
-            @foreach($headerHtml as $vo)
-                <div class="pb-4">
-                    {!! $vo !!}
+<div class="p-5">
+    @if($title || $actions)
+        <div class="flex items-center  pb-4">
+            @if($title)
+                <div class="flex-grow">
+                    <div class="text-xl">{{$title}}</div>
                 </div>
-            @endforeach
-        @endif
+            @endif
+            @if ($actions)
+                <div class="flex-none flex gap-2">
+                    @foreach ($actions as $vo)
+                        <div>{!! $vo !!}</div>
+                    @endforeach
+                </div>
+            @endif
+        </div>
+    @endif
 
-        <div class="bg-white rounded {{!$dialog ? 'px-4 shadow' : ''}}">
+    @if($headerHtml)
+        @foreach($headerHtml as $vo)
+            <div class="pb-4">
+                {!! $vo !!}
+            </div>
+        @endforeach
+    @endif
 
-            @if($quick || $actions || $filterType)
-                <form method="get" data-filter>
-                    @if($filterParams || $quick || $actions)
-                        <div x-cloak x-data="{collapse: {{$filterStatus ? 'true' : 'false'}} }">
+    <div class="lg:flex gap-4">
+        @foreach($sideLeftHtml as $vo)
+            <div class="lg:flex-none hidden lg:block">
+                {!! $vo !!}
+            </div>
+        @endforeach
 
-                            @if ($filterType)
-                                <div class="border-b border-gray-300 flex gap-4 flex-nowrap overflow-x-auto">
-                                    {!! $filterType !!}
-                                </div>
-                            @endif
+        <div class=" lg:flex-grow">
 
-                            @foreach($filterParams as $vo)
-                                <input type="hidden" name="{{$vo['name']}}" value="{{$vo['value']}}">
-                            @endforeach
-                            @if ($quick)
-                                <div class="flex flex-wrap flex-col lg:flex-row gap-4 pt-4">
-                                    @foreach ($quick as $vo)
-                                        {!! $vo !!}
-                                    @endforeach
+            <div class="bg-white rounded {{!$dialog ? 'px-4 shadow' : ''}}">
 
-                                    @if ($filter)
+                @if($quick || $actions || $filterType)
+                    <form method="get" data-filter>
+                        @if($filterParams || $quick || $actions)
+                            <div x-cloak x-data="{collapse: {{$filterStatus ? 'true' : 'false'}} }">
+
+                                @if ($filterType)
+                                    <div class="border-b border-gray-300 flex gap-4 flex-nowrap overflow-x-auto">
+                                        {!! $filterType !!}
+                                    </div>
+                                @endif
+
+                                @foreach($filterParams as $vo)
+                                    <input type="hidden" name="{{$vo['name']}}" value="{{$vo['value']}}">
+                                @endforeach
+                                @if ($quick)
+                                    <div class="flex flex-wrap flex-col lg:flex-row gap-4 pt-4">
+                                        @foreach ($quick as $vo)
+                                            {!! $vo !!}
+                                        @endforeach
+
+                                        @if ($filter)
+                                            <div>
+                                                <button type="button"
+                                                        class="btn-outline-blue  w-full flex justify-center"
+                                                        data-bs-toggle="collapse" data-bs-target="#filter-sub"
+                                                        aria-expanded="false" aria-label="筛选"
+                                                        @click="collapse = !collapse">
+                                                    筛选
+                                                </button>
+                                            </div>
+                                        @endif
                                         <div>
-                                            <button type="button" class="btn-outline-blue  w-full flex justify-center"
-                                                    data-bs-toggle="collapse" data-bs-target="#filter-sub"
-                                                    aria-expanded="false" aria-label="筛选" @click="collapse = !collapse">
-                                                筛选
+                                            <button type="submit" class=" btn-blue w-full flex justify-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                     style="width: 1.25rem; height: 1.25rem"
+                                                     width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                     stroke="currentColor" fill="none" stroke-linecap="round"
+                                                     stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                    <circle cx="10" cy="10" r="7"/>
+                                                    <line x1="21" y1="21" x2="15" y2="15"/>
+                                                </svg>
+
                                             </button>
                                         </div>
-                                    @endif
-                                    <div>
-                                        <button type="submit" class=" btn-blue w-full flex justify-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                 style="width: 1.25rem; height: 1.25rem"
-                                                 width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                 stroke="currentColor" fill="none" stroke-linecap="round"
-                                                 stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                <circle cx="10" cy="10" r="7"/>
-                                                <line x1="21" y1="21" x2="15" y2="15"/>
-                                            </svg>
+                                    </div>
+                                @endif
 
-                                        </button>
+                                <div class=" {{ $filterStatus ? 'show' : '' }}" x-show="collapse">
+                                    <div class="flex flex-wrap flex-col lg:flex-row gap-4 pt-4">
+                                        @foreach ($filter as $vo)
+                                            {!! $vo !!}
+                                        @endforeach
                                     </div>
                                 </div>
-                            @endif
+                            </div>
+                        @endif
+                    </form>
+                @endif
 
-                            <div class=" {{ $filterStatus ? 'show' : '' }}" x-show="collapse">
-                                <div class="flex flex-wrap flex-col lg:flex-row gap-4 pt-4">
-                                    @foreach ($filter as $vo)
-                                        {!! $vo !!}
+
+                <div data-js="table-bind" data-ajax="{{$ajax}}" data-tree="{{$tree}}" data-sortable="{{$sortable}}"
+                     class="{{!$dialog ? 'py-4' : ''}}" id="table">
+                    @if($toolsHtml)
+                        @foreach($toolsHtml as $vo)
+                            {!! $vo !!}
+                        @endforeach
+                    @endif
+                    <div class="overflow-x-auto">
+                        @if($tree)
+                            <div class="py-4 table-list" data-table>
+                                @if(!$ajax)
+                                    @include('vendor.duxphp.duxravel-app.src.core.UI.View.table-list')
+                                @endif
+                            </div>
+                        @else
+                            <table class="table-box {{$class}}" style="{!! $style !!}" {!! $attr !!}
+                            data-table>
+                                @if(!$ajax)
+                                    @include('vendor.duxphp.duxravel-app.src.core.UI.View.table-ajax')
+                                @endif
+                            </table>
+                        @endif
+                    </div>
+                    @if($batch || $pages)
+                        <div class="card-footer flex items-center pt-2">
+                            <div class="flex flex-grow space-x-2" data-batch>
+                                @if($batch)
+                                    <div class="flex items-center ">
+                                        <input class="form-check-input align-middle" data-check-all type="checkbox">
+                                    </div>
+                                    @foreach($batch as $vo)
+                                        <div>
+                                            {!! $vo !!}
+                                        </div>
                                     @endforeach
-                                </div>
+                                @endif
+                            </div>
+                            <div class="flex-none">
+                                @if($ajax)
+                                    <ul data-pagination class="pagination m-0"></ul>
+                                @else
+                                    {!! $pages !!}
+                                @endif
                             </div>
                         </div>
-                    @endif
-                </form>
-            @endif
 
+                </div>
+                @endif
 
-            <div data-js="table-bind" data-ajax="{{$ajax}}" data-tree="{{$tree}}" data-sortable="{{$sortable}}"
-                 class="{{!$dialog ? 'py-4' : ''}}" id="table">
-                @if($toolsHtml)
-                    @foreach($toolsHtml as $vo)
+                @if($footerHtml)
+                    @foreach($footerHtml as $vo)
                         {!! $vo !!}
                     @endforeach
                 @endif
-                <div class="overflow-x-auto">
-                    @if($tree)
-                        <div class="py-4 table-list" data-table>
-                            @if(!$ajax)
-                                @include('vendor.duxphp.duxravel-app.src.core.UI.View.table-list')
-                            @endif
-                        </div>
-                    @else
-                        <table class="table-box {{$class}}" style="{!! $style !!}" {!! $attr !!}
-                        data-table>
-                            @if(!$ajax)
-                                @include('vendor.duxphp.duxravel-app.src.core.UI.View.table-ajax')
-                            @endif
-                        </table>
-                    @endif
-                </div>
-                @if($batch || $pages)
-                    <div class="card-footer flex items-center pt-2">
-                        <div class="flex flex-grow space-x-2" data-batch>
-                            @if($batch)
-                                <div class="flex items-center ">
-                                    <input class="form-check-input align-middle" data-check-all type="checkbox">
-                                </div>
-                                @foreach($batch as $vo)
-                                    <div>
-                                        {!! $vo !!}
-                                    </div>
-                                @endforeach
-                            @endif
-                        </div>
-                        <div class="flex-none">
-                            @if($ajax)
-                                <ul data-pagination class="pagination m-0"></ul>
-                            @else
-                                {!! $pages !!}
-                            @endif
-                        </div>
-                    </div>
-
             </div>
-            @endif
-
-            @if($footerHtml)
-                @foreach($footerHtml as $vo)
-                    {!! $vo !!}
-                @endforeach
-            @endif
-
         </div>
+
+        @foreach($sideRightHtml as $vo)
+            <div class="lg:flex-none hidden lg:block">
+                {!! $vo !!}
+            </div>
+        @endforeach
+
     </div>
-
-    @foreach($sideRightHtml as $vo)
-        <div class="lg:flex-none hidden lg:block">
-            {!! $vo !!}
-        </div>
-    @endforeach
-
 </div>
