e78e9eae1613c4091d642a0cdd63ca9bc3d39fda
Mr.L <admin@duxphp.com>
2021-07-20 11:28:35 +0800

    增加跨域中间件

--- src/core/Config/cors.php
+++ src/core/Config/cors.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
