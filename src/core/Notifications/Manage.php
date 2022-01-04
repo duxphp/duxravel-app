c30e966710da920c691be6a0ec56f177e3c82a9f
Mr.L <admin@duxphp.com>
2022-01-04 13:20:29 +0800

    增加管理通知

---
+++ src/core/Notifications/Manage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Duxravel\Core\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class Manage extends Notification
+{
+    use Queueable;
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * @param $notifiable
+     * @return string[]
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * @param $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'invoice' => $this->invoice,
+        ];
+    }
+}
