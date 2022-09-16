c5a5aa890b0f9911c272065b39d6ee9ebb0058a7
shockerli <shockerli@upfor.club>
2022-09-16 22:58:35 +0800

    Status组件支持label自定义

--- src/core/UI/Table/Column/Status.php
+++ src/core/UI/Table/Column/Status.php
@@ -15,15 +15,15 @@ class Status implements Component
     private string $type;
 
     /**
-     * @param array $map
-     * @param array $color
-     * @param string $type
+     * @param  array  $map
+     * @param  array  $color
+     * @param  string $type
      */
     public function __construct(array $map, array $color, string $type = 'badge')
     {
-        $this->map = $map;
+        $this->map   = $map;
         $this->color = $color;
-        $this->type = $type;
+        $this->type  = $type;
     }
 
     /**
@@ -34,22 +34,26 @@ class Status implements Component
     {
         $statusArr = [];
         foreach ($this->map as $key => $vo) {
-            $statusArr[$key]['name'] = $vo;
+            $statusArr[$key]['name']  = $vo;
             $statusArr[$key]['color'] = $this->color[$key] ?? 'blue';
         }
+
+        // 可自定义label
+        $labelIf = count(explode('.', $label)) > 1 ? $label : "rowData.record['{$label}']";
 
         $node = [];
         foreach ($statusArr as $key => $vo) {
             if ($this->type === 'badge') {
                 $item = (new Badge($vo['name']))->color($vo['color'])->render();
-                $item['vIf'] = "rowData.record['{$label}'] == " . (is_numeric($key) ? $key : "'$key'");
             } else {
                 $item = [
                     'nodeName' => 'div',
-                    'class' => 'text-' . $vo['color'] . '-900',
-                    'child' => $vo['name']
+                    'class'    => 'text-' . $vo['color'] . '-900',
+                    'child'    => $vo['name'],
                 ];
             }
+            $item['vIf'] = $labelIf . " == " . (is_numeric($key) ? $key : "'$key'");
+
             $node[] = $item;
         }
         return $node;
