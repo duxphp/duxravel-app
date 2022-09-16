e6eb940967e7e0521a3a355b2066d19668e5954e
shockerli <shockerli@upfor.club>
2022-09-16 18:00:03 +0800

    `Lists` 组件添加size和addItem方法

--- src/core/UI/Widget/Lists.php
+++ src/core/UI/Widget/Lists.php
@@ -2,28 +2,54 @@
 
 namespace Duxravel\Core\UI\Widget;
 
-use Duxravel\Core\UI\Tools;
-
 /**
  * Class Lists
+ *
+ * @see [arco.design] https://arco.design/vue/component/list
  * @package Duxravel\Core\UI\Widget
  */
 class Lists extends Widget
 {
 
-    private array $data;
+    private array $data = [];
+
+    private string $size = 'medium'; // 尺寸大小: small,medium,large
 
     /**
      * Lists constructor.
-     * @param               $data
-     * @param callable|null $callback
+     *
+     * @param  array         $data
+     * @param  callable|null $callback
      */
-    public function __construct($data, callable $callback = NULL)
+    public function __construct($data = [], callable $callback = null)
     {
-        $this->data = $data;
+        $this->data     = $data;
         $this->callback = $callback;
     }
 
+    /**
+     * 尺寸大小
+     *
+     * @param  string $size
+     * @return $this
+     */
+    public function size(string $size = ''): self
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * 添加普通项
+     *
+     * @param  scalar|array $item
+     * @return $this
+     */
+    public function addItem($item)
+    {
+        $this->data[] = $item;
+        return $this;
+    }
 
     /**
      * @return array
@@ -32,17 +58,18 @@ class Lists extends Widget
     {
 
         $inner = [];
-        $i = 0;
+        $i     = 0;
         foreach ($this->data as $item) {
             $inner[] = [
                 'nodeName' => 'a-list-item',
-                'child' => $item
+                'child'    => $item,
             ];
         }
 
         return $inner ? [
             'nodeName' => 'a-list',
-            'child' => $inner
+            'size'     => $this->size,
+            'child'    => $inner,
         ] : [
             'nodeName' => 'a-empty',
         ];
