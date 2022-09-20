4ae6fe94de2c08349567484f22de5e3873fb6759
shockerli <shockerli@upfor.club>
2022-09-20 09:48:31 +0800

    修复: 表格行内编辑（Input）

--- src/core/UI/Table/Column/Input.php
+++ src/core/UI/Table/Column/Input.php
@@ -2,10 +2,8 @@
 
 namespace Duxravel\Core\UI\Table\Column;
 
-use Duxravel\Core\UI\Tools;
-
 /**
- * Class Input
+ * 表格行数输入框（实时编辑）
  */
 class Input implements Component
 {
@@ -14,26 +12,66 @@ class Input implements Component
     private array $params;
     private string $field;
     private array $fields;
+    private string $size = 'small';
+    private bool $allowClear = false;
+    private array $attr = [];
 
     /**
-     * @param string $field
-     * @param string $route
-     * @param array  $params
+     * @param  string $field
+     * @param  string $route
+     * @param  array  $params
      */
     public function __construct(string $field, string $route, array $params = [])
     {
-        $this->field = $field;
+        $this->field  = $field;
         $this->params = $params;
-        $this->route = $route;
+        $this->route  = $route;
     }
 
     /**
-     * @param array $fields
+     * @param  array $fields
      * @return $this
      */
-    public function fields(array $fields = []): Input
+    public function fields(array $fields = [])
     {
         $this->fields = $fields;
+        return $this;
+    }
+
+    /**
+     * 输入框大小
+     *
+     * @param  string $size 'mini' | 'small' | 'medium' | 'large'
+     * @return $this
+     */
+    public function size($size)
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * 设置附加属性
+     *
+     * @param  string $name
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function attr(string $name, $value)
+    {
+        $this->attr[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * 是否允许清空输入框
+     *
+     * @param  bool $allowClear
+     * @return $this
+     */
+    public function allowClear($allowClear)
+    {
+        $this->allowClear = (bool)$allowClear;
         return $this;
     }
 
@@ -43,13 +81,17 @@ class Input implements Component
      */
     public function render($label): array
     {
-        $url = app_route($this->route, $this->params, false, 'rowData.record', $this->fields);
-        return [
-            'nodeName' => 'n-input',
-            'class' => 'shadow-sm',
-            'vModel:value' => "rowData.record['$label']",
-            'vOn:blur' => "editValue($url, {'field': '$this->field', '$this->field': rowData.record['$label']})",
+        $url  = app_route($this->route, $this->params, false, 'rowData.record', $this->fields);
+        $node = [
+            'nodeName'          => 'a-input',
+            'size'              => $this->size,
+            'allowClear'        => $this->allowClear,
+            'style'             => 'padding: 0px 6px',
+            'vModel:modelValue' => "rowData.record.$label",
+            'vOn:blur'          => "rowData.record.$label = \$event.target._value, editValue($url, {'field': '$this->field', '$this->field': rowData.record.$label})",
         ];
+
+        return array_merge($node, (array)$this->attr);
     }
 
 }
